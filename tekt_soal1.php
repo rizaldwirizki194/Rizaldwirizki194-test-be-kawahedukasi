a9e4ccf83663acc53d11e98dc1d369829da10f56
rizaldwirizki194 <98677288+rizaldwirizki194@users.noreply.github.com>
2022-07-12 20:53:12 -0700

    Update tekt_soal1.php

--- tekt_soal1.php
+++ tekt_soal1.php
@@ -13,7 +13,7 @@
 
 $data = [1, 2, 4, 3, 10, 11, 20, 5, 100, 200];
 
-for{$i = 0; $i < count($data); $i++) {
+for($i = 0; $i < count($data); $i++) {
   if($data[$i] % 5 == 0 && $data[$i] % 11 == 0) {
     echo "FizzBuzz";
   } else if($data[$i] % 5 == 0) {
@@ -25,3 +25,4 @@ for{$i = 0; $i < count($data); $i++) {
   }
   echo "\n";
 }
+?>
